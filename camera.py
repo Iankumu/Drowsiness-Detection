d2b42810cd23b8d347e169de78f8074e04168b4c
Ian Kumu <ikariuki741@gmail.com>
2021-11-13 21:03:03 +0300

    First Commit

---
+++ camera.py
@@ -0,0 +1,135 @@
+import cv2
+import mediapipe as mp
+from keras.models import load_model
+import utils
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_face_mesh = mp.solutions.face_mesh
+
+label=['Close','Open']
+
+model = load_model('models/drowsiness_new.h5')
+
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+count=0
+score=0
+rpred=[99]
+lpred=[99]
+
+CLOSED_EYES_FRAME = 3
+CEF_COUNTER = 0
+TOTAL_BLINKS = 0
+
+Open_frames = 0
+Closed_frames = 0
+# 0-yawn, 1-no_yawn, 2-Closed, 3-Open
+
+class Video(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        self.Closed_frames = Closed_frames
+        self.Open_frames = Open_frames
+        self.count = count
+        self.score = score
+        self.label = label
+        self.rpred = rpred
+        self.lpred = lpred
+        self.CEF_COUNTER = CEF_COUNTER
+        self.CLOSED_EYES_FRAME = CLOSED_EYES_FRAME
+        self.TOTAL_BLINKS = TOTAL_BLINKS
+
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        with mp_face_mesh.FaceMesh(
+        max_num_faces=1,
+        refine_landmarks=True,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as face_mesh:
+
+            
+            ret,image = self.video.read()
+            height,width = image.shape[:2]
+
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = face_mesh.process(image)
+
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.multi_face_landmarks:
+                for face_landmarks in results.multi_face_landmarks:
+                    mesh_coords = utils.landmarksDetection(image,results,False)
+
+                    image = utils.fillPolyTrans(image,[mesh_coords[p] for p in utils.LEFT_EYE],utils.GREEN,opacity=0.3)
+                    image = utils.fillPolyTrans(image,[mesh_coords[p] for p in utils.RIGHT_EYE],utils.GREEN,opacity=0.3)
+
+                    # Getting the Blink Ratio
+                    ratio = utils.blinkRatio(image,mesh_coords,utils.RIGHT_EYE,utils.LEFT_EYE)
+                    cv2.putText(image, f'ratio {ratio}', (100, 100),
+                            font, 1.0, utils.GREEN, 2)
+                    if ratio > 4.9:
+                        self.CEF_COUNTER += 1 
+                    cv2.putText(image, 'Blinks', (200, 30),font, 1.3, utils.PINK, 2)
+
+                    if self.CEF_COUNTER>self.CLOSED_EYES_FRAME:
+                        self.TOTAL_BLINKS +=1
+                        self.CEF_COUNTER = 0 
+                    cv2.putText(image, f'Total Blinks {self.TOTAL_BLINKS}', (100, 150),font, 0.6, utils.PINK, 2)                  
+
+                    right_coords = [mesh_coords[p] for p in utils.RIGHT_EYE]
+                    left_coords = [mesh_coords[p] for p in utils.LEFT_EYE]
+
+                    # Check if eyes are open or closed
+                    crop_right, crop_left = utils.eyesExtractor(image, right_coords, left_coords)
+
+                    # cv2.imshow("right",crop_right)
+                    # cv2.imshow("left",crop_left)
+
+                    for x in crop_right:
+                        self.count=self.count+1
+                        r_eye = cv2.cvtColor(crop_right,cv2.IMREAD_COLOR)
+                        r_eye = cv2.resize(r_eye,(145,145))
+                        r_eye= r_eye/255
+                        r_eye=  r_eye.reshape(-1,145,145,3)
+                        self.rpred = np.argmax(model.predict(r_eye))
+                        if(self.rpred==3):
+                            self.label='Open' 
+                        if(rpred==2):
+                            self.label='Closed'
+                        break
+                    for y in crop_left:
+                        self.count=self.count+1
+                        l_eye = cv2.cvtColor(crop_left,cv2.IMREAD_COLOR)    
+                        l_eye = cv2.resize(l_eye,(145,145))
+                        l_eye= l_eye/255
+                        l_eye=l_eye.reshape(-1,145,145,3)
+                        self.lpred = np.argmax(model.predict(l_eye))
+                        if(self.lpred==3):
+                            self.label='Open'   
+                        if(self.lpred==2):
+                            self.label='Closed'
+                        break
+                    
+                   
+                    if(self.rpred==2 and self.lpred==2):
+                        self.score=self.score+1
+                        self.Closed_frames += int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
+                        cv2.putText(image,"Closed",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+                
+                    else:
+                        self.score=self.score-1
+                        self.Open_frames += int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
+                        cv2.putText(image,"Open",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+                    if(self.score<0):
+                        self.score=0   
+                    cv2.putText(image,'Score:'+str(self.score),(100,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+
+                    # Getting the PERCLOS
+                    print("PERCLOS: " + str(utils.perclos(self.Closed_frames,self.Open_frames)))
+                    
+            ret,jpg = cv2.imencode('.jpg',image)
+            return jpg.tobytes()
---
+++ utils.py
@@ -0,0 +1,167 @@
+from scipy.spatial import distance
+import math
+import playsound,cv2
+import numpy as np
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (255,0,0)
+RED = (0,0,255)
+CYAN = (255,255,0)
+YELLOW =(0,255,255)
+MAGENTA = (255,0,255)
+GRAY = (128,128,128)
+GREEN = (0,255,0)
+PURPLE = (128,0,128)
+ORANGE = (0,165,255)
+PINK = (147,20,255)
+color=(225, 0, 100)
+
+# face bounder indices 
+FACE_OVAL=[ 10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288, 397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136, 172, 58, 132, 93, 234, 127, 162, 21, 54, 103,67, 109]
+
+# Left eyes indices 
+LEFT_EYE =[ 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398 ]
+LEFT_EYEBROW =[ 336, 296, 334, 293, 300, 276, 283, 282, 295, 285 ]
+
+# right eyes indices
+RIGHT_EYE=[ 33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246 ]  
+RIGHT_EYEBROW=[ 70, 63, 105, 66, 107, 55, 65, 52, 53, 46 ]
+
+# lips indices for Landmarks
+LIPS=[ 61, 146, 91, 181, 84, 17, 314, 405, 321, 375,291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95,185, 40, 39, 37,0 ,267 ,269 ,270 ,409, 415, 310, 311, 312, 13, 82, 81, 42, 183, 78 ]
+LOWER_LIPS =[61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95]
+UPPER_LIPS=[ 185, 40, 39, 37,0 ,267 ,269 ,270 ,409, 415, 310, 311, 312, 13, 82, 81, 42, 183, 78] 
+
+
+# Eucledian Distance
+def eucledianDistance(point,point1):
+  x,y  =point
+  x1,y1  =point1
+  distance = math.sqrt((x1-x)**2 +(y1-y)**2)
+  return distance
+
+# Blink Ratio 
+
+def blinkRatio(img,landmarks,right_indices,left_indices):
+  # Right Eye Horizontal Plane
+  rh_right = landmarks[right_indices[0]]
+  rh_left=landmarks[right_indices[8]]
+
+  # Right Eye vertical Plane
+  rv_top = landmarks[right_indices[12]]
+  rv_bottom=landmarks[right_indices[4]]
+
+   # Left Eye Horizontal Plane
+  lh_right = landmarks[left_indices[0]]
+  lh_left=landmarks[left_indices[8]]
+
+  # Left Eye vertical Plane
+  lv_top = landmarks[left_indices[12]]
+  lv_bottom=landmarks[left_indices[4]]
+
+  rhDistance = eucledianDistance(rh_right,rh_left)
+  rvDistance = eucledianDistance(rv_top,rv_bottom)
+
+  lhDistance = eucledianDistance(lh_right,lh_left)
+  lvDistance = eucledianDistance(lv_top,lv_bottom)
+
+  reRatio = rhDistance/rvDistance
+  leRatio = lhDistance/lvDistance
+
+  ratio = (reRatio+leRatio)/2
+  return ratio
+
+# Eyes Extractor function,
+def eyesExtractor(img, right_eye_coords, left_eye_coords):
+    # converting color image to  scale image 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # getting the dimension of image 
+    dim = gray.shape
+
+    # creating mask from gray scale dim
+    mask = np.zeros(dim, dtype=np.uint8)
+
+    # drawing Eyes Shape on mask with white color 
+    cv2.fillPoly(mask, [np.array(right_eye_coords, dtype=np.int32)], 255)
+    cv2.fillPoly(mask, [np.array(left_eye_coords, dtype=np.int32)], 255)
+
+    # showing the mask 
+    # cv2.imshow('mask', mask)
+    
+    # draw eyes image on mask, where white shape is the eyes
+    eyes = cv2.bitwise_and(gray, gray, mask=mask)
+    # change black color to gray 
+    # cv2.imshow('eyes draw', eyes)
+    eyes[mask==0]=155
+    
+    # getting minium and maximum x and y  for right and left eyes 
+    # For Right Eye 
+    r_max_x = (max(right_eye_coords, key=lambda item: item[0]))[0]
+    r_min_x = (min(right_eye_coords, key=lambda item: item[0]))[0]
+    r_max_y = (max(right_eye_coords, key=lambda item : item[1]))[1]
+    r_min_y = (min(right_eye_coords, key=lambda item: item[1]))[1]
+
+    # For LEFT Eye
+    l_max_x = (max(left_eye_coords, key=lambda item: item[0]))[0]
+    l_min_x = (min(left_eye_coords, key=lambda item: item[0]))[0]
+    l_max_y = (max(left_eye_coords, key=lambda item : item[1]))[1]
+    l_min_y = (min(left_eye_coords, key=lambda item: item[1]))[1]
+
+    # croping the eyes from mask 
+    cropped_right = eyes[r_min_y: r_max_y, r_min_x: r_max_x]
+    cropped_left = eyes[l_min_y: l_max_y, l_min_x: l_max_x]
+
+    # returning the cropped eyes 
+    return cropped_right, cropped_left
+
+# play an alarm sound
+def sound_alarm(path):
+	playsound.playsound(path)
+
+# Get Pupil Circularity
+# def circularity(eye):
+#     A = distance.euclidean(eye[1], eye[4])
+#     radius  = A/2.0
+#     Area = math.pi * (radius ** 2)
+#     p = 0
+#     p += distance.euclidean(eye[0], eye[1])
+#     p += distance.euclidean(eye[1], eye[2])
+#     p += distance.euclidean(eye[2], eye[3])
+#     p += distance.euclidean(eye[3], eye[4])
+#     p += distance.euclidean(eye[4], eye[5])
+#     p += distance.euclidean(eye[5], eye[0])
+#     return 4 * math.pi * Area /(p**2)
+
+# PERCLOS
+def perclos(frame_closed,frame_open):
+  perclos = frame_closed/(frame_open+frame_closed) * 100
+  return perclos
+
+
+# landmark detection function
+def landmarksDetection(img,results,draw=False):
+    img_height,img_width =img.shape[:2]
+    mesh_coords = [(int(point.x * img_width),(int(point.y*img_height))) for point in results.multi_face_landmarks[0].landmark] 
+    if draw:
+        [cv2.circle(img,p,2,color,-1) for p in mesh_coords]
+
+    return mesh_coords
+
+def fillPolyTrans(img, points, color, opacity):
+    """
+    @param img: (mat) input image, where shape is drawn.
+    @param points: list [tuples(int, int) these are the points custom shape,FillPoly
+    @param color: (tuples (int, int, int)
+    @param opacity:  it is transparency of image.
+    @return: img(mat) image with rectangle draw.
+    """
+    list_to_np_array = np.array(points, dtype=np.int32)
+    overlay = img.copy()  # coping the image
+    cv2.fillPoly(overlay,[list_to_np_array], color )
+    new_img = cv2.addWeighted(overlay, opacity, img, 1 - opacity, 0)
+    # print(points_list)
+    img = new_img
+    cv2.polylines(img, [list_to_np_array], True, color,1, cv2.LINE_AA)
+    return img
